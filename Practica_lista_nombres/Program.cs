9bb5b02f1579d76c3f46a2e5ee9767c73e1cdf04
GJAA98 <79035604+GJAA98@users.noreply.github.com>
2021-03-01 17:04:36 -0400

    Add files via upload
    Un programa que prepare espacio para un máximo de 100 nombres. El usuario deberá ir introduciendo un nombre cada vez, hasta que se pulse Intro sin teclear nada, momento en el que dejarán de pedirse más nombres y se mostrará en pantalla la lista de los nombres que se han introducido.

---
+++ Practica_lista_nombres/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Practica_lista_nombres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nombres = new string[100];
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Inserte un nombre:");
+                string nombre = Console.ReadLine();
+                if(nombre == "")
+                {
+                    break;
+                }
+                nombres[i] = nombre;
+            }
+
+            Console.WriteLine("Lista de nombres:");
+            for (int i = 0; i < 100; i++)
+            {
+                if (nombres[i] == null)
+                {
+                    break;
+                }
+                Console.WriteLine(nombres[i]);
+            }
+
+        }
+    }
+}
